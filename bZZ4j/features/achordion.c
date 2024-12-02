9b5d1f0b936efbc0e5518f092318ebb1312f30bd
anycancellable <>
2024-12-03 00:36:03 +0100

    enable ACHORDION_STREAK

--- bZZ4j/features/achordion.c
+++ bZZ4j/features/achordion.c
@@ -345,7 +345,7 @@ __attribute__((weak)) uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
 
 // By default, Shift and Ctrl mods are eager, and Alt and GUI are not.
 __attribute__((weak)) bool achordion_eager_mod(uint8_t mod) {
-  return false;
+  return (mod & (MOD_LALT | MOD_LGUI)) == 0;
 }
 
 #ifdef ACHORDION_STREAK
